b35c627c63ede3550ec1de9348ab8868298f0895
anand2312 <40204976+anand2312@users.noreply.github.com>
2023-06-04 19:11:33 +0530

    Make basic WASM wrapper around automaton-evaluator functionality

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,19 +1,43 @@
-mod utils;
-
+use std::collections::{HashMap, HashSet};
+use serde::{Serialize, Deserialize};
+// mod utils;
 use wasm_bindgen::prelude::*;
+use automaton_evaluator::nfa::NFA;
+use automaton_evaluator::traits::Acceptor;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+// We only use the NFA implementation here; that should work for DFA's too.
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct WasmNFA {
+    // we need to put the wasm_bindgen attribute on everything that needs to be exposed to JS
+    // i didn't wanna pollute the library with wasm stuff, so I'm just wrapping the library
+    // functionality in new methods/structs here that have the wasm_bindgen attribute set
+    nfa: NFA
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct NfaArgs {
+    pub states: HashMap<usize, String>,
+    pub initial_state: usize,
+    pub final_states: HashSet<usize>,
+    pub transitions: Vec<Vec<Vec<char>>>
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
+impl WasmNFA {
+    #[wasm_bindgen(constructor)]
+    pub fn new(args: JsValue) -> Self {
+        let nfa_args: NfaArgs = serde_wasm_bindgen::from_value(args).unwrap();
+        Self {
+            nfa: NFA::new(nfa_args.states, nfa_args.initial_state, nfa_args.final_states, nfa_args.transitions)
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn test_string(&self, s: String) -> bool {
+        self.nfa.test_string(s)
+    }
 }
