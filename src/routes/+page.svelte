dfa1fef246507e10ed39d1b50bf689566a11212a
Rishabh Chopde <rishikumarikuf@gmail.com>
2023-06-11 20:24:37 +0530

    Intial run

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
 <h1>FLA</h1>
 <p>Inspired by JFLAP (the Java Formal Languages and Automata Program), but I didn't wanna name this FLAW (the FLA website) :p</p>
 
+<h6>Now with a new contributor :3</h6>
+<a href="canvas/">To the canvas!</a>
---
+++ src/routes/canvas/+page.svelte
@@ -0,0 +1,2 @@
+<h1>WIP</h1>
+<!-- add a <canvas/> here -->
